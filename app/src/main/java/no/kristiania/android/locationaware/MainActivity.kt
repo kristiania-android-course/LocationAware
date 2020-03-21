9bdf5de27faf8792b2e1ca8b3a88c84840f01605
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-21 22:13:53 +0100

    android Location aware initial commit

--- app/src/main/java/no/kristiania/android/locationaware/MainActivity.kt
+++ app/src/main/java/no/kristiania/android/locationaware/MainActivity.kt
@@ -1,17 +1,22 @@
 package no.kristiania.android.locationaware
 
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.*
+import java.util.*
 
+
+private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS: Long = UPDATE_INTERVAL_IN_MILLISECONDS/2
 
 class MainActivity : AppCompatActivity() {
 
-    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
-    private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS: Long = UPDATE_INTERVAL_IN_MILLISECONDS/2
+
     private lateinit var mLocationRequest: LocationRequest
     private lateinit var mLocationCallback: LocationCallback
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
@@ -45,12 +50,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onNewLocation(lastLocation: Location) {
-        runOnUiThread {
-            Toast.makeText(
-                    this,
-                    "${lastLocation.latitude.toString()} and ${lastLocation.longitude}",
-                    Toast.LENGTH_SHORT ).show()
-        }
+        // GeoCoder api to ge the address from the location data.
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1)
+        val returnedAddress: Address = addresses[0]
+
+        Toast.makeText(
+            this,
+            "${lastLocation.latitude.toString()} and ${lastLocation.longitude} ${returnedAddress.createStringAddress()}",
+            Toast.LENGTH_SHORT ).show()
 
     }
 
@@ -69,6 +77,15 @@ class MainActivity : AppCompatActivity() {
             fastestInterval = FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+    }
+
+    // Extension function to create string address from the address object.
+    fun Address.createStringAddress() : String {
+        val strReturnedAddress = StringBuilder()
+        for (i in 0..maxAddressLineIndex) {
+            strReturnedAddress.append(getAddressLine(i)).append("\n")
+        }
+        return strReturnedAddress.toString()
     }
 
 
