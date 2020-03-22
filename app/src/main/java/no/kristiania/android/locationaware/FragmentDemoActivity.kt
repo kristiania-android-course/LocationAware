c72b9d675f3d277f7d86ecbeaf5a9b6e24d7bf48
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-22 18:52:15 +0100

    live finisehd

--- app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
+++ app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
@@ -2,20 +2,17 @@ package no.kristiania.android.locationaware
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import no.kristiania.android.locationaware.fragments.FirstFragment
 
 class FragmentDemoActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frgment_demo)
-        supportFragmentManager.beginTransaction().add(R.id.frame, FirstFragment(), "firt").commit()
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
     }
-
 }
