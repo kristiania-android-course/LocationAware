be751b7d9885d61f6e383a6ee30f31ff174797c7
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-22 15:02:11 +0100

    Maps

--- app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
+++ app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
@@ -1,16 +1,104 @@
 package no.kristiania.android.locationaware
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class FragmentDemoActivity : AppCompatActivity() {
+class FragmentDemoActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapFragment: SupportMapFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frgment_demo)
+
+        if (permissionCheck()) {
+            setMapFragment()
+        }
+
+    }
+
+    private fun setMapFragment() {
+        mapFragment = SupportMapFragment()
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.first_frame, FirstFragment(), "First")
+            .add(R.id.first_frame, mapFragment, "First")
             .commit()
+        mapFragment.getMapAsync(this)
+    }
+
+
+    private fun permissionCheck(): Boolean =
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            promptForPermission()
+            false
+        } else {
+            true
+        }
+
+    // Request for permission  show the dialog window to approve the usage of location feature
+    private fun promptForPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            1111
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1111 -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setMapFragment()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Sorry, the app cant do anything with out the location permission.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    override fun onMapReady(mMap: GoogleMap) {
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+
+
+        mMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+
+        val cameraPosition =
+            CameraPosition.Builder().target(sydney).zoom(12.0f).build()
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+
+        mMap.moveCamera(cameraUpdate)
     }
 }
