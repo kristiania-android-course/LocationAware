fb9b53b9a924de7def66b2567f880c240c6f6016
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-22 20:16:28 +0100

    live finisehd

--- app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
+++ app/src/main/java/no/kristiania/android/locationaware/FragmentDemoActivity.kt
@@ -7,29 +7,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 
-class FragmentDemoActivity : AppCompatActivity(), OnMapReadyCallback {
+class FragmentDemoActivity : AppCompatActivity() {
 
 
-    private lateinit var mMap: GoogleMap
-
-    private lateinit var mapFragment: SupportMapFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frgment_demo)
 
-        mapFragment = SupportMapFragment()
-        mapFragment.getMapAsync(this)
-
-        if (permissionCheck()) {
-            supportFragmentManager.beginTransaction().add(R.id.frame, mapFragment, "map").commit()
-        }
 
     }
 
@@ -65,8 +51,7 @@ class FragmentDemoActivity : AppCompatActivity(), OnMapReadyCallback {
         when (requestCode) {
             1111 -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    supportFragmentManager.beginTransaction().add(R.id.frame, mapFragment, "map")
-                        .commit()
+
                 } else {
                     Toast.makeText(
                         this,
@@ -76,15 +61,6 @@ class FragmentDemoActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        this.mMap = googleMap
-        mMap.isMyLocationEnabled = true
-        mMap.uiSettings.isMyLocationButtonEnabled = true
-        val update = CameraUpdateFactory.newLatLngZoom(LatLng(59.9812, 10.742), 13.0f)
-        mMap.moveCamera(update)
-
     }
 
 }
